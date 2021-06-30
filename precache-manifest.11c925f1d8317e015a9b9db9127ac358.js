1dbbb8e338752b5cc95941c33544b40c16665fcf
Parth Pathak <pathakparth359@gmail.com>
2021-06-30 11:27:57 +0530

    Updates

---
+++ precache-manifest.11c925f1d8317e015a9b9db9127ac358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456b4ce3015bb695b9c98eb3bb5651a3",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "68cf00ea9bcdf48e4edf",
+    "url": "/reactwebsite/static/css/2.660505ec.chunk.css"
+  },
+  {
+    "revision": "932ec02963b8fa5a66ef",
+    "url": "/reactwebsite/static/css/main.06c6e3f1.chunk.css"
+  },
+  {
+    "revision": "68cf00ea9bcdf48e4edf",
+    "url": "/reactwebsite/static/js/2.4e20afc0.chunk.js"
+  },
+  {
+    "revision": "91ff2e69adb4b0e67019d5ef58035ec9",
+    "url": "/reactwebsite/static/js/2.4e20afc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932ec02963b8fa5a66ef",
+    "url": "/reactwebsite/static/js/main.8bb3bafe.chunk.js"
+  },
+  {
+    "revision": "efc88d0a922e04ad1430",
+    "url": "/reactwebsite/static/js/runtime-main.c3f14d51.js"
+  }
+]);
